1dfc722ffee2b377fc76a094e35501b550efc903
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-05 19:42:23 +0900

    put setSize for designate size on boardWidth, boardHeight

--- App.java
+++ App.java
@@ -7,6 +7,7 @@ public class App {
 
         JFrame frame = new JFrame("Flappy Bird");
         frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

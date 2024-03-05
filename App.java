e9fa3c6810831affe66968672d9c873999720001
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-05 19:43:32 +0900

    modify value on int boardHeight

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) throws Exception {
         int boardWidth = 360;
-        int boardHeight = 360;
+        int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
         frame.setVisible(true);

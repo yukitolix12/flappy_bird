9e26482001ea781641e62cfde964fa9d9f4c942a
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-05 19:27:39 +0900

    put JFlame and designate title,"Flappy Bird" in new JFlame

--- App.java
+++ App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         int boardWidth = 360;
         int boardHeight = 360;
+
+        JFrame frame = new JFrame("Flappy Bird");
     }
 }

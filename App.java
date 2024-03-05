4cc38517f8ff5d7286cb9b26b4f465bc12e2d90c
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-05 19:25:17 +0900

    define boardWidth and boardHeight variable forms

--- App.java
+++ App.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        int boardWidth = 360;
+        int boardHeight = 360;
     }
 }

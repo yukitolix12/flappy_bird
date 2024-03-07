50486c8adad13a35bfe3a3e4ee12663e00683a50
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-07 20:15:56 +0900

    put 4 images object to change background

--- FlappyBird.java
+++ FlappyBird.java
@@ -8,6 +8,14 @@ public class FlappyBird extends JPanel {
     int boardWidth = 360;
     int boardHeight = 640;
 
+    //images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+
+
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.blue);

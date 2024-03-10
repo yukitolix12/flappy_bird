45f4642940f5c7bfeaf65697dca268dfbaf34bd8
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-10 20:42:25 +0900

    define three functions to click on quick fix

--- FlappyBird.java
+++ FlappyBird.java
@@ -83,4 +83,22 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         move();
         repaint();
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
 }

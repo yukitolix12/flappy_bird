57b2530674d87eb9458355dc81f44326439d9ba1
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-11 20:36:21 +0900

    put flappyBird.requestFocus to set visibility

--- App.java
+++ App.java
@@ -15,6 +15,7 @@ public class App {
         FlappyBird flappyBird = new FlappyBird();
         frame.add(flappyBird);
         frame.pack();
+        flappyBird.requestFocus();
         frame.setVisible(true);
     }
 }

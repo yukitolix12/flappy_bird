34496155d848c7b45131ff900289a43e2dcf985e
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-14 20:10:20 +0900

    topPipe.Y  changes to randomPipeY

--- FlappyBird.java
+++ FlappyBird.java
@@ -100,6 +100,7 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
         int randomPipeY = (int )(pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2));
         Pipe topPipe = new Pipe(topPipeImg);
+        topPipe.Y = randomPipeY;
         pipes.add(topPipe);
     }
 

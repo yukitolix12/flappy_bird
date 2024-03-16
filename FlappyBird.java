89743c7cff458ac24e3762ce46c1f63ddd085ebb
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-03-16 14:45:27 +0900

    fix typo in formula

--- FlappyBird.java
+++ FlappyBird.java
@@ -154,7 +154,7 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
     public boolean collision(Bird a, Pipe b){
         return a.x < b.x + b.width && //a's top left corner doesn't reach b's top right corner
-               a.x + b.width > b.x && //a's top right corner passes b's top left corner
+               a.x + a.width > b.x && //a's top right corner passes b's top left corner
                a.y < b.y + b.height && //a's top left corner doesn't reach b's bottom left corner
                a.y + b.height > b.y;  //a's bottom left corner passes b's top left corner
     }
